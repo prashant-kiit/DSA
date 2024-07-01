3acccfd3d1fd6dac0ba3d7d6304797a247d1702b
Prashant Kumar Singh <prashantsingh090798@gmail.com>
2024-07-01 17:15:37 +0530

    added: bridge

---
+++ Java Program/Design_Pattern/Bridge.java
@@ -0,0 +1,71 @@
+package Design_Pattern;
+
+public class Bridge {
+    public static void main(String[] args) {
+        Player player = new Hotstar("Star Wars", new _4K());
+        player.play();
+    }
+}
+
+abstract class Player {
+    protected String movie = null;
+    protected Processor processor = null;
+
+    public abstract void play();
+}
+
+class Youtube extends Player {
+    public Youtube(String movie, Processor processor) {
+        this.movie = movie;
+        this.processor = processor;
+    }
+
+    @Override
+    public void play() {
+        this.processor.process("Youtube", this.movie);
+    }
+}
+
+class Netflix extends Player {
+    public Netflix(String movie, Processor processor) {
+        this.movie = movie;
+        this.processor = processor;
+    }
+
+    @Override
+    public void play() {
+        this.processor.process("Netflix", this.movie);
+    }
+}
+
+class Hotstar extends Player {
+    public Hotstar(String movie, Processor processor) {
+        this.movie = movie;
+        this.processor = processor;
+    }
+
+    @Override
+    public void play() {
+        this.processor.process("Hotstar", this.movie);
+    }
+}
+
+abstract class Processor {
+    public abstract void process(String platform, String movie);
+}
+
+class HD extends Processor {
+    @Override
+    public void process(String platform, String movie) {
+        System.out.println("Movie processed into HD movie");
+        System.out.println(platform + " is playing movie " + movie);
+    }
+}
+
+class _4K extends Processor {
+    @Override
+    public void process(String platform, String movie) {
+        System.out.println("Movie processed into 4K movie");
+        System.out.println(platform + " is playing movie " + movie);
+    }
+}
--- Java Program/Design_Pattern/Hiding.java
+++ Java Program/Design_Pattern/Hiding.java
@@ -4,7 +4,7 @@ public class Hiding {
     public static void main(String[] args) {
         B b = new B();
         b.show();
-        b.show(1);
+        b.show(1);  
     }
 }
 
